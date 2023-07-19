9085f17c87722690e535b1668c65878c83ed2d39
adoppp <121878757+adoppp@users.noreply.github.com>
2023-07-19 20:25:57 +0200

    pushhh

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,48 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+import { Section } from './Section/Section';
+import { FeedbackOptions } from './Feedback/Feedback';
+import { Statistics } from './Statistics/Statistics';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = state => {
+    this.setState(prevValue => ({
+      [state]: prevValue[state] + 1,
+    }));
+  };
+
+  totalValue() {
+    let total = Object.values(this.state).reduce((prevValue, number) => {
+      return prevValue + number;
+    }, 0);
+  };
+
+  countPositiveFeedbackPercentage({ good }) {
+    const total = this.totalValue();
+    let positive = total !== 0 ? Math.round((good / total) * 100) : 0;
+    return positive;
+  }
+
+    render() {
+      const { good, neutral, bad } = this.state;
+      const options = Object.keys(this.state);
+      const total = this.totalValue();
+      const positive = this.countPositiveFeedbackPercentage(this.state, total);
+      return (
+        <div>
+          <Section title="Please leave feedback">
+            <FeedbackOptions onLeaveFeedback={this.onLeaveFeedback} options={options}></FeedbackOptions>
+          </Section>
+          <Section title="Statisctics">
+            <Statistics good={good} neutral={neutral} bad={bad} total={total} positive={positive}></Statistics>
+          </Section>
+        </div>
+      );
+    };
+  };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+
+export const Statistics = ({ good, neutral, bad, total, positive }) => { 
+    return (
+        <div>
+            <p>Good: {good}</p>
+            <p>Neutral: {neutral}</p>
+            <p>Bad: {bad}</p>
+            <p>Total: {total}</p>
+            <p>Positive: {positive}</p>
+        </div>
+    );
+};
+
+Statistics.propTypes = {
+    good: PropTypes.number.isRequired,
+    neutral: PropTypes.number.isRequired,
+    bad: PropTypes.number.isRequired,
+    total: PropTypes.number.isRequired,
+    positive: PropTypes.number.isRequired,
+}
